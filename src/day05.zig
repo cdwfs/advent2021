92fafaf47ccaf1c21a1c650b4226153eef6dafa7
Cort <1944792+cdwfs@users.noreply.github.com>
2021-12-08 05:04:40 -0800

    formatting

--- src/day05.zig
+++ src/day05.zig
@@ -4,8 +4,8 @@ const util = @import("util.zig");
 const data = @embedFile("../data/day05.txt");
 
 const Point2 = struct {
-    x:i16,
-    y:i16,
+    x: i16,
+    y: i16,
 };
 const VentMap = struct {
     map: std.AutoHashMap(Point2, u32) = undefined,
@@ -22,7 +22,7 @@ const VentMap = struct {
         self.* = undefined;
     }
 
-    pub fn add(self: *@This(), point:Point2) void {
+    pub fn add(self: *@This(), point: Point2) void {
         if (self.map.getOrPut(point)) |result| {
             if (!result.found_existing) {
                 result.value_ptr.* = 0;
@@ -35,14 +35,14 @@ const VentMap = struct {
 };
 
 const VentLine = struct {
-    x1:i16,
-    y1:i16,
-    x2:i16,
-    y2:i16,
+    x1: i16,
+    y1: i16,
+    x2: i16,
+    y2: i16,
 };
 const Input = struct {
     lines: std.ArrayList(VentLine) = std.ArrayList(VentLine).init(std.testing.allocator),
-    pub fn deinit(self:@This()) void {
+    pub fn deinit(self: @This()) void {
         self.lines.deinit();
     }
 };
@@ -50,7 +50,7 @@ const Input = struct {
 fn parseInput(input_text: []const u8) Input {
     var input = Input{};
     var lines = std.mem.tokenize(u8, input_text, "\r\n");
-    while(lines.next()) |line| {
+    while (lines.next()) |line| {
         var endpoints = std.mem.tokenize(u8, line, ",- >");
         input.lines.append(VentLine{
             .x1 = parseInt(i16, endpoints.next().?, 10) catch unreachable,
@@ -66,30 +66,30 @@ fn part1(input: Input) i64 {
     var map = VentMap.init(std.testing.allocator);
     defer map.deinit();
 
-    for(input.lines.items) |line| {
+    for (input.lines.items) |line| {
         //print("line from {d},{d} to {d},{d}\n", .{line.x1, line.y1, line.x2, line.y2});
         if (line.x1 == line.x2) {
             const y_min = min(line.y1, line.y2);
             const y_max = max(line.y1, line.y2);
             var y = y_min;
-            while(y <= y_max) : (y += 1) {
+            while (y <= y_max) : (y += 1) {
                 //print(" point at {d},{d}\n", .{line.x1, y});
-                map.add(Point2{.x = line.x1, .y = y});
+                map.add(Point2{ .x = line.x1, .y = y });
             }
         } else if (line.y1 == line.y2) {
             const x_min = min(line.x1, line.x2);
             const x_max = max(line.x1, line.x2);
             var x = x_min;
-            while(x <= x_max) : (x += 1) {
+            while (x <= x_max) : (x += 1) {
                 //print(" point at {d},{d}\n", .{x, line.y1});
-                map.add(Point2{.x = x, .y = line.y1});
+                map.add(Point2{ .x = x, .y = line.y1 });
             }
         }
     }
 
     var values = map.map.valueIterator();
-    var count:i64 = 0;
-    while(values.next()) |val| {
+    var count: i64 = 0;
+    while (values.next()) |val| {
         if (val.* > 1) {
             count += 1;
         }
@@ -101,33 +101,36 @@ fn part2(input: Input) i64 {
     var map = VentMap.init(std.testing.allocator);
     defer map.deinit();
 
-    for(input.lines.items) |line| {
+    for (input.lines.items) |line| {
         //print("line from {d},{d} to {d},{d}\n", .{line.x1, line.y1, line.x2, line.y2});
         var x = line.x1;
         var y = line.y1;
-        var dx:i16 = 0;
+        var dx: i16 = 0;
         if (line.x2 > line.x1) {
             dx = 1;
         } else if (line.x2 < line.x1) {
             dx = -1;
         }
-        var dy:i16 = 0;
+        var dy: i16 = 0;
         if (line.y2 > line.y1) {
             dy = 1;
         } else if (line.y2 < line.y1) {
             dy = -1;
         }
-        while(x != line.x2 or y != line.y2)  : ({x += dx; y += dy;}) {
+        while (x != line.x2 or y != line.y2) : ({
+            x += dx;
+            y += dy;
+        }) {
             //print(" point at {d},{d}\n", .{x, y});
-            map.add(Point2{.x = x, .y = y});
+            map.add(Point2{ .x = x, .y = y });
         }
         //print(" point at {d},{d}\n", .{line.x2, line.y2});
-        map.add(Point2{.x = line.x2, .y = line.y2});
+        map.add(Point2{ .x = line.x2, .y = line.y2 });
     }
 
     var values = map.map.valueIterator();
-    var count:i64 = 0;
-    while(values.next()) |val| {
+    var count: i64 = 0;
+    while (values.next()) |val| {
         if (val.* > 1) {
             count += 1;
         }
--- src/day06.zig
+++ src/day06.zig
@@ -4,8 +4,8 @@ const util = @import("util.zig");
 const data = @embedFile("../data/day06.txt");
 
 const Input = struct {
-    fish_ages:std.ArrayList(u8) = std.ArrayList(u8).init(std.testing.allocator),
-    pub fn deinit(self:@This()) void {
+    fish_ages: std.ArrayList(u8) = std.ArrayList(u8).init(std.testing.allocator),
+    pub fn deinit(self: @This()) void {
         self.fish_ages.deinit();
     }
 };
@@ -15,27 +15,27 @@ fn parseInput(input_text: []const u8) !Input {
     errdefer input.deinit();
 
     var ages = std.mem.tokenize(u8, input_text, ",\r\n");
-    while(ages.next()) |age| {
+    while (ages.next()) |age| {
         try input.fish_ages.append(try parseInt(u8, age, 10));
     }
     return input;
 }
 
-fn simulateFish(input: Input, day_count:usize) i64 {
-    var fish_at_age:[9]i64 = .{0} ** 9;
-    for(input.fish_ages.items) |age| {
+fn simulateFish(input: Input, day_count: usize) i64 {
+    var fish_at_age: [9]i64 = .{0} ** 9;
+    for (input.fish_ages.items) |age| {
         fish_at_age[age] += 1;
     }
-    var day:usize = 0;
-    while(day < day_count) : (day += 1) {
+    var day: usize = 0;
+    while (day < day_count) : (day += 1) {
         const parent_count = fish_at_age[0];
         std.mem.rotate(i64, fish_at_age[0..], 1);
         fish_at_age[8] = parent_count;
         fish_at_age[6] += parent_count;
     }
-    
-    var sum:i64 = 0;
-    for(fish_at_age) |fish_count| {
+
+    var sum: i64 = 0;
+    for (fish_at_age) |fish_count| {
         sum += fish_count;
     }
     return sum;
@@ -50,10 +50,10 @@ fn part2(input: Input) i64 {
 }
 
 const test_data = "3,4,3,1,2";
-const part1_test_solution:?i64 = 5934;
-const part1_solution:?i64 = 388739;
-const part2_test_solution:?i64 = 26984457539;
-const part2_solution:?i64 = 1741362314973;
+const part1_test_solution: ?i64 = 5934;
+const part1_solution: ?i64 = 388739;
+const part2_test_solution: ?i64 = 26984457539;
+const part2_solution: ?i64 = 1741362314973;
 
 fn testPart1() !void {
     var test_input = try parseInput(test_data);
